63c67d184d0b2ff6b02642a1ef9e937a89058467
cocoz2027 <cocoz2027@student.cis.edu.hk>
2024-08-27 14:34:53 +0800

    finished lab 1

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your age?");
+        int age = scanner.nextInt();
+        int dateage = 7 + age/2;
+        System.out.println(age + "-year olds should date somebody who is at least " + dateage + " years old.");
+
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your age");
+        int age = scanner.nextInt();
+        int page = 100 - age;
+        System.out.println(age + "-year olds should read at least " + page + " pages before giving up on a book.");
     }
 }
